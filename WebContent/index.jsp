e7d5c4e3e3a810733a6289fa04066ad781c75e18
Amanda <amanda@Amandas-MacBook-Pro.local>
2017-04-24 17:36:22 -0500

    Initial commit

---
+++ WebContent/index.jsp
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Movies Application: Index</title>
+		<meta name="description" content="This is an MVC web application example 
+		that demonstrates how to use IO to output the contents of an Excel 
+		spreadsheet to a web page.">
+<%@ include file="includes/styles.jsp" %>
+	</head>
+	<body>
+		<div class="container">
+			<div class="hero-unit">
+				<h1>Welcome to the Movies Application</h1>
+			</div>
+<%@ include file="includes/navigation.jsp" %>
+			<div class="container">
+				<p>This is a movies application where you can look up movies
+				to see who directed them and find out how long they are.</p>
+			</div>
+<%@ include file="includes/footer.jsp" %>
+		</div>
+<%@ include file="includes/scripts.jsp" %>
+	</body>
+</html>
---
+++ src/main/java/edu/cvtc/web/model/Movie.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package edu.cvtc.web.model;
+
+import java.io.Serializable;
+
+/**
+ * @author amanda
+ *
+ */
+public class Movie implements Serializable {
+	
+	private static final long serialVersionUID = 3132582901663673344L;
+	
+	private String title;
+	private String director;
+	private Integer lengthInMinutes;
+	
+	public Movie(){
+	}
+	
+	public Movie(String title, String director, Integer lengthInMinutes) {
+		this.title = title;
+		this.director = director;
+		this.lengthInMinutes = lengthInMinutes;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public Integer getLengthInMinutes() {
+		return lengthInMinutes;
+	}
+
+	public void setLengthInMinutes(Integer lengthInMinutes) {
+		this.lengthInMinutes = lengthInMinutes;
+	}
+	
+	
+
+}
